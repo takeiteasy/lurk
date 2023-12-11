c9871d841ae15d813db6dbc90f55b046aefe3b81
George Watson <gigolo@hotmail.co.uk>
2023-12-11 22:33:29 +0000

    working test

--- game/menu.c
+++ game/menu.c
@@ -16,19 +16,28 @@ static void deinit(gkState* state, GameKit *scene) {
 }
 
 static void reload(gkState* state, GameKit *scene) {
-    
+
 }
 
 static void unload(gkState* state, GameKit *scene) {
-    
+
 }
 
 static void event(gkState* state, GameKit *scene, const sapp_event *e) {
-    
+
 }
 
 static void frame(gkState* state, GameKit *scene, float delta) {
-    
+    int width  = gkWindowWidth(state),
+        height = gkWindowHeight(state);
+    float ratio = (float)width / (float)height;
+    gkViewport(state, 0, 0, width, height);
+    gkProject(state, -ratio, ratio, 1.f, -1.f);
+    gkSetColor(state, 1.f, 0.f, 0.f, 1.f);
+    gkClear(state);
+
+    gkSetColor(state, 0.f, 0.f, 1.f, 1.f);
+    gkDrawFilledRect(state, -.5f, -.5f, 1.f, 1.f);
 }
 
 EXPORT const gkScene scene = {
